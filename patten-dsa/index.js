40c624a5b6d5a6018f7e558e2cfe9b5a0d2aa04b
KunjPatel1712 <8413kunjpatel@gmail.com>
2025-08-20 13:23:14 +0530

    Add files via upload

---
+++ patten-dsa/index.js
@@ -0,0 +1,142 @@
+//q-1
+
+// for (let i = 1; i <= 5; i++) {
+//     for (let j = 1; j <= i; j++) {
+//         document.writeln(j);
+//     }
+//     document.writeln("<br>");
+// }
+
+//q-2
+
+// for(let i =1; i<=5;i++)
+// {
+//     for(let j=i; j>=1 ;j--){
+//         document.writeln(j)
+//     }
+//     document.writeln("<br>")
+// }
+
+//q-3
+
+// for(let i=1; i<=5; i++)
+// {
+//     for(let j=1; j<=i; j++){
+//         document.writeln(i)
+//     }
+//     document.writeln("<br>")
+// }
+
+//q-4
+
+// for(let i=5; i>=1; i--)
+// {
+//     for(let j=i; j<=5;j++ )
+//     {
+//         document.writeln(j)
+//     }
+//     document.writeln("<br>")
+// }
+
+//q-5
+// for (let i = 5; i >= 1; i--)
+//      {
+//     for (let j = i; j <= 5; j++)
+//      {
+//         document.writeln(j);
+//     }
+//     document.writeln("<br>");
+// }
+
+//q-6
+
+// for(let i=5 ; i>=1 ; i--)
+// {
+//     for(let j=5; j>=i; j--)
+//     {
+//         document.writeln(i)
+//     }
+//     document.writeln("<br>")
+// }
+
+//q-7
+
+
+// for (let i = 5; i >= 1; i--) {
+//     for (let j = 1; j <= i; j++) {
+//         document.writeln(j);
+//     }
+//     document.writeln("<br>");
+// }
+
+//q-8
+
+// for (let i = 1; i <= 5; i++)
+//      {
+//     for (let j = i; j <= 5; j++) 
+//         {
+//         document.writeln(j );
+//     }
+//     document.writeln("<br>");
+// }
+
+//q-9
+
+// for(let i=1; i<=5; i++)
+// {
+//     for(let j=5; j>=i; j--)
+//     {
+//         document.writeln(i)
+//     }
+//     document.writeln("<br>")
+// }
+
+//q-10
+
+// for (let i = 1; i <= 5; i++) {
+//     for (let j = 5; j >= i; j--) {
+//         document.writeln(j );
+//     }
+//     document.writeln("<br>");
+// }
+
+//q-11
+
+// for (let i = 5; i >= 1; i--) {
+//     for (let j = i; j >= 1; j--) {
+//         document.writeln(j );
+//     }
+//     document.writeln("<br>");
+// }
+
+//q-12
+
+// for(let i=5; i>=1; i--)
+// {
+//     for(let j=1; j<=i; j++){
+//         document.writeln(i)
+//     }
+//     document.writeln("<br>")
+// }
+
+//q-13
+
+// for (let i = 1; i <= 5; i++) {
+//     for (let j = 1; j <= i; j++) {
+//         document.writeln("* ");
+//     }
+//     document.writeln("<br>");
+// }
+
+//q-14
+
+//     for (let i = 5; i >= 1; i--) {
+//     for (let j = 1; j <= i; j++) {
+//         document.writeln("* ");
+//     }
+//     document.writeln("<br>");
+// }
+
+
+//q-15
+
